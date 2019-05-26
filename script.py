1382a12a528e0a822cdbadd6ba3fd9c33f8cde1b
Nana Yaw <yaw.owusu@hotmail.com>
2019-05-26 11:44:12 +0100

    First Commit - Author Reset

---
+++ script.py
@@ -0,0 +1,19 @@
+# import sys
+# import requests
+
+# print(sys.version)
+# print(sys.executable)
+
+
+# def greet(who_to_greet):
+#     greeting = "Hello, {}".format(who_to_greet)
+#     return greeting
+
+# r = requests.get("https://coreyms.com")
+# print(greet("World"))
+# print(greet("Nana Yaw"))
+# print()
+# print(r.status_code)
+
+name = input("Your Name? ")
+print("Hello, ", name)
